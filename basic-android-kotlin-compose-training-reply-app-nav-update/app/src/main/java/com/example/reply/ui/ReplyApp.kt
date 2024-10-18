a0f960e45a7bac239a1bea1f0b40b4e3203dbb5f
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2024-10-17 21:36:28 -0600

    Lo que hicimos en este commit fue la vista previa para los diferentes tamaños de pantalla

--- basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -1,28 +1,36 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.reply.ui
 
+import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply.data.Email
 import com.example.reply.data.MailboxType
+import com.example.reply.ui.theme.ReplyTheme
 import com.example.reply.ui.utils.ReplyNavigationType
+
+@Preview(showBackground = true, widthDp = 700)
+@Composable
+fun ReplyAppMediumPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Medium)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 1000)
+@Composable
+fun ReplyAppExpandedPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
+        }
+    }
+}
 
 @Composable
 fun ReplyApp(
@@ -47,6 +55,7 @@ fun ReplyApp(
             navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
     }
+
     ReplyHomeScreen(
         navigationType = navigationType,
         replyUiState = replyUiState,
@@ -55,9 +64,7 @@ fun ReplyApp(
             viewModel.resetHomeScreenStates()
         },
         onEmailCardPressed = { email: Email ->
-            viewModel.updateDetailsScreenStates(
-                email = email
-            )
+            viewModel.updateDetailsScreenStates(email = email)
         },
         onDetailScreenBackPressed = {
             viewModel.resetHomeScreenStates()
