8bb516b4e400f57495055a047ded7952e0933d54
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2024-10-18 00:38:20 -0600

    se configuro la prueba para el tamaño de pantalla compacta

---
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/utils/ReplyAppTest.kt
@@ -0,0 +1,21 @@
+package com.example.reply.ui.utils
+
+class ReplyAppTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_verifyUsingBottomNavigation() {
+        // Set up compact window
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact
+            )
+        }
+        // Bottom navigation is displayed
+        composeTestRule.onNodeWithTagForStringId(
+            R.string.navigation_bottom
+        ).assertExists()
+    }
+}
